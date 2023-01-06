6f62f152787c70319d1fc1446b2de8ae4cab9864
Thomas Perronin <thomas.perronin@blablacar.com>
2023-01-06 11:07:46 +0100

    feat(nvim): add used lsp languages

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -3,10 +3,12 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-  'tsserver',
-  'eslint',
   'sumneko_lua',
-  'rust_analyzer',
+  'gopls',
+  'terraformls',
+  'yamlls',
+  'dockerls',
+  'jsonls',
 })
 
 -- Fix Undefined global 'vim'
