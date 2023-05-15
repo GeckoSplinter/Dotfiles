8ea7df20d703531050b05c2e577e0ae31feb6ac6
Thomas Perronin <thomas.perronin@blablacar.com>
2023-05-15 18:58:40 +0200

    feat(add NvChad-custom): move to NvChad

---
+++ NvChad-custom/mappings.lua
@@ -0,0 +1,48 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    -- git status from fugitive
+    ["<leader>gs"] = { vim.cmd.Git, "Git status" },
+  },
+}
+
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+--vim.keymap.set("n", "<leader>gp", ":Git -c push.default=current push<CR>")
+--vim.keymap.set("n", "<leader>gpf", ":Git -c push.default=current push -f<CR>")
+vim.keymap.set("n", "<leader>gu", ":Git pull --rebase origin master<CR>")
+vim.keymap.set("n", "<leader>ga", "<cmd>!git fetch --all<CR>")
+vim.keymap.set("n", "<leader>gh", ":<cmd>!gh pr create -f <CR>")
+
+local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
+
+local autocmd = vim.api.nvim_create_autocmd
+autocmd("BufWinEnter", {
+  group = ThePrimeagen_Fugitive,
+  pattern = "*",
+  callback = function()
+    if vim.bo.ft ~= "fugitive" then
+      return
+    end
+
+    local bufnr = vim.api.nvim_get_current_buf()
+    local opts = { buffer = bufnr, remap = false }
+    vim.keymap.set("n", "<leader>p", function()
+      vim.cmd.Git "push"
+    end, opts)
+
+    -- rebase always
+    vim.keymap.set("n", "<leader>P", function()
+      vim.cmd.Git "pull --rebase"
+    end, opts)
+
+    -- NOTE: It allows me to easily set the branch i am pushing and any tracking
+    -- needed if i did not set the branch up correctly
+    vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts)
+  end,
+})
+-- more keybinds!
+
+return M
