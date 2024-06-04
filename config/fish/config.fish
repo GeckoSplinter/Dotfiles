d513420b9fa221f099b3190b870d374709442e23
Gecko Splinter <gecko.splinter@gmail.com>
2024-06-04 22:25:29 +0200

    feat(fish): update config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,22 +4,42 @@ set -x LANG en_US.UTF-8
 set -x EDITOR nvim
 set -x VIRTUAL_ENV_DISABLE_PROMPT 1
 alias vim 'nvim'
+set -x SPACEFISH_KUBECONTEXT_SYMBOL "⎈ "
 
 # PATH
 set -x PATH "/usr/local/sbin" $PATH
 set -x PATH "$HOME/ops-bin" $PATH
+fish_add_path /opt/homebrew/bin
+fish_add_path $HOME/.composer/vendor/bin
+fish_add_path /opt/homebrew/opt/openvpn/sbin
+fish_add_path $HOME/.rd/bin
 
 # Golang config
 set -x GOPATH $HOME/go
 set -x PATH $GOPATH/bin $PATH
 set -x PATH $HOME/.krew/bin $PATH
 
+# Yabai disable mouse for fusion
+function yabai_disable_mouse
+    yabai -m config mouse_follows_focus off
+    yabai -m config focus_follows_mouse off
+end
+function yabai_enable_mouse
+    yabai -m config mouse_follows_focus on
+    yabai -m config focus_follows_mouse autofocus
+end
+
 # Rust config
 set -x PATH "$HOME/.cargo/bin" $PATH
 
 # GCLOUD
-#set -g -x "CLOUDSDK_PYTHON" "/usr/local/opt/python@3.8/libexec/bin/python"
-#source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc"
+source "$(brew --prefix)/share/google-cloud-sdk/path.fish.inc"
+
+# SOPS
+set -x SOPS_AGE_KEY_FILE "$HOME/.config/sops/age/keys.txt"
+
+# Kubernetes
+set -gx PATH $PATH $HOME/.krew/bin
 
 # Theme
 set -g theme_nerd_fonts yes
@@ -29,6 +49,11 @@ set -g theme_display_k8s_namespace yes
 # Work stuff
 # # TODO migrate to omf plugin
 source $HOME/.config/fish/work.fish
+source $HOME/.config/fish/helper.fish
+
+# Defaut browser
+alias dbc='osascript $HOME/Perso/Dotfiles/macos-helper/setDefaultBrowser.scpt chrome'
+alias dbb='osascript $HOME/Perso/Dotfiles/macos-helper/setDefaultBrowser.scpt browser'
 
 
 abbr -a glola "git log --graph --pretty=format:'%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --all"
@@ -39,12 +64,7 @@ abbr -a k "kubectl"
 abbr -a kp "kubectl get pods"
 abbr -a ka "kubectl get all,cm,secret,sa"
 
+abbr -a s "kitten ssh"
 
-# SSH alias
-alias s "kitty +kitten ssh"
-
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    eval (keychain --quiet --eval --agents ssh,gpg t.perronin.id_ed25519 67B539D168A141B3)
-    starship init fish | source
-end
+starship init fish | source
+zoxide init fish | source
