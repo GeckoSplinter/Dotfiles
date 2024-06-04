50a15fa0a9b14a86f312d629b278fcbf2a599a68
Gecko Splinter <gecko.splinter@gmail.com>
2024-06-04 23:21:52 +0200

    feat(fish): add config

---
+++ config/fish/helper.fish
@@ -0,0 +1,7 @@
+# Writing a fish shell function to setup a local git config for username email and signingkey
+
+function git_setup_local
+    git config --local user.name "Gecko Splinter"
+    git config --local user.email "gecko.splinter@gmail.com"
+    git config --local user.signingkey C99015B62BBEB6FE
+end
