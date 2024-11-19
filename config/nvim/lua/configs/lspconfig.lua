a1ea24e9a5fda94a048cce8bd65553adcda6b2ad
Thomas (Gecko) Perronin <thomas.perronin@blablacar.com>
2024-11-19 09:48:23 +0100

    feat(nvim): update the conf with latest versions

--- config/nvim/lua/configs/lspconfig.lua
+++ config/nvim/lua/configs/lspconfig.lua
@@ -1,23 +1,63 @@
--- EXAMPLE 
-local on_attach = require("nvchad.configs.lspconfig").on_attach
-local on_init = require("nvchad.configs.lspconfig").on_init
-local capabilities = require("nvchad.configs.lspconfig").capabilities
+local nvlsp = require("nvchad.configs.lspconfig")
+local lspconfig = require("lspconfig")
 
-local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
+nvlsp.defaults() -- loads nvchad's defaults
+
+local defaultServers = {
+  "terraformls",
+  "dockerls",
+  "pyright",
+}
 
 -- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-  }
+for _, lsp in ipairs(defaultServers) do
+	lspconfig[lsp].setup({
+		on_attach = nvlsp.on_attach,
+		on_init = nvlsp.on_init,
+		capabilities = nvlsp.capabilities,
+	})
 end
 
--- typescript
-lspconfig.tsserver.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
+lspconfig.helm_ls.setup({
+	on_attach = nvlsp.on_attach,
+	capabilities = nvlsp.capabilities,
+	settings = {
+		["helm-ls"] = {
+			-- logLevel = "debug",
+			yamlls = {
+				path = "yaml-language-server",
+			},
+		},
+	},
+})
+
+lspconfig.yamlls.setup({
+	on_attach = nvlsp.on_attach,
+	capabilities = nvlsp.capabilities,
+	settings = {
+		yaml = {
+			format = {
+				enable = true,
+			},
+			validate = true,
+			hover = true,
+			completion = true,
+			schemaStore = {
+				enable = true,
+			},
+		},
+	},
+})
+
+lspconfig.gopls.setup({
+	on_attach = nvlsp.on_attach,
+	capabilities = nvlsp.capabilities,
+	settings = {
+		gopls = {
+			analyses = {
+				unusedparams = true,
+			},
+			staticcheck = true,
+		},
+	},
+})
--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -7,32 +7,34 @@ return {
     end,
   },
 
-  -- These are some examples, uncomment them if you want to see them work!
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
-  --
-  -- {
-  -- 	"williamboman/mason.nvim",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"lua-language-server", "stylua",
-  -- 			"html-lsp", "css-lsp" , "prettier"
-  -- 		},
-  -- 	},
-  -- },
-  --
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+  	"williamboman/mason.nvim",
+  	opts = {
+  		ensure_installed = {
+  			"lua-language-server", "stylua",
+  			"html-lsp", "css-lsp" , "prettier",
+        "terraform-ls", "gopls",
+        "yamlls"
+  		},
+  	},
+  },
+
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+        "terraform", "yaml", "helm",
+        "go", "java"
+  		},
+  	},
+  },
 }
